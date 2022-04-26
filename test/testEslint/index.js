84a8497803221884487d114ff6e80bd1626a44e1
lutianwen <tom_lu@yunquna.com>
2022-04-26 14:34:18 +0800

    添加依赖

---
+++ test/testEslint/index.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: 鲁田文
+ * @Date: 2022-04-26 10:58:54
+ * @LastEditTime: 2022-04-26 14:33:09
+ * @LastEditors: 鲁田文
+ * @Description: 
+ */
+import React from 'react'
+
+export default function Index() {
+
+  useEffect(() => {
+                      a = a + 1
+  }, []);
+  return (
+    <div>index</div>
+  )
+}
+
