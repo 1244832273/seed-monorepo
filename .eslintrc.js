fb2d4cfb1d253dd87c8ba5e225b713b42a0c8a9a
lutianwen <tom_lu@yunquna.com>
2022-04-28 15:06:41 +0800

    webpack-dev包初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,6 @@
 module.exports = {
   extends: ['@seed/eslint-config-seed'],
   rules: {
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
     '@typescript-eslint/no-var-requires': 'off',
   },
 };
---
+++ packages/seed/seed-webpack-dev/index.js
@@ -0,0 +1,45 @@
+// 环境变量配置
+process.env.NODE_ENV = 'development';
+process.env.SEED_ENV = 'development';
+
+const path = require('path');
+const { merge } = require('webpack-merge');
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+
+const getWebpackConfig = require('@seed/webpack-config');
+console.log('getWebpackConfig', getWebpackConfig());
+
+const devConfig = {
+  mode: 'development',
+  output: {
+    publicPath: '/',
+  },
+  plugins: [new ErrorOverlayPlugin()],
+  devtool: 'cheap-module-source-map',
+  cache: {
+    type: 'filesystem', // 使用文件缓存
+  },
+  devServer: {
+    // static允许我们在DevServer下访问该目录的静态资源
+    // 简单理解来说 当我们启动DevServer时相当于启动了一个本地服务器
+    // 这个服务器会同时以static-directory目录作为跟路径启动
+    // 这样的话就可以访问到static/directory下的资源了
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    historyApiFallback: {
+      // disableDotRule: true,
+      index: '/',
+    },
+    // 默认为true
+    hot: true,
+    // 是否开启代码压缩
+    compress: false,
+    // 启动的端口
+    port: 9000,
+  },
+};
+
+const webpackConfigDefault = getWebpackConfig();
+
+module.exports = merge(webpackConfigDefault, devConfig);
